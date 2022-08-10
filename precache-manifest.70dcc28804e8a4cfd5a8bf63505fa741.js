5c6e4d67ac8f76a03e4f1c71f207c1e8a8428e00
alexstanza <alexander.stanza@gmail.com>
2022-08-11 01:07:35 +0800

    Deploy Parkingly App ver 0.3.0

---
+++ precache-manifest.70dcc28804e8a4cfd5a8bf63505fa741.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f31b28f26709dde0f8",
+    "url": "/static/css/main.6dd61769.chunk.css"
+  },
+  {
+    "revision": "32f31b28f26709dde0f8",
+    "url": "/static/js/main.1460f5a8.chunk.js"
+  },
+  {
+    "revision": "b22b5f536f8126263b01",
+    "url": "/static/js/runtime~main.1b1cf5ad.js"
+  },
+  {
+    "revision": "cd5a63f044126b43c318",
+    "url": "/static/css/2.128eb78d.chunk.css"
+  },
+  {
+    "revision": "cd5a63f044126b43c318",
+    "url": "/static/js/2.b1226af0.chunk.js"
+  },
+  {
+    "revision": "7da80a4b2c1fc8dd018d",
+    "url": "/static/js/3.e3b4af8e.chunk.js"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "25cdd92655b3e391868f74fc05f445c2",
+    "url": "/index.html"
+  }
+];
